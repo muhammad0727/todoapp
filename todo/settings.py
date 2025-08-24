5432fde2f60203c8ada2e7661b940815443092b9
muhammad0727 <150504367+muhammad0727@users.noreply.github.com>
2025-08-24 12:58:43 +0500

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -80,21 +80,28 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import os
+import dj_database_url
+
+# ... other settings
+
+# Configure the database
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
-'''
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL",  f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-        conn_max_age=600,
-        #ssl_require=True
-    )
-}
+else:
+    # Fallback to local development database settings
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
