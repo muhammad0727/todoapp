5510be8c6cb98f1146211e5903603b5aa110e5cb
muhammad0727 <150504367+muhammad0727@users.noreply.github.com>
2025-08-24 11:56:43 +0500

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -138,3 +138,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://todoapp-production-e78e.up.railway.app",
+]
