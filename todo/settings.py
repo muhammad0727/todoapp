1bf56f27ff69bd0712860127cd4e19bd0c622530
muhammad0727 <150504367+muhammad0727@users.noreply.github.com>
2025-08-24 12:21:33 +0500

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -90,6 +90,7 @@ DATABASES = {
 '''
 DATABASES = {
     'default': dj_database_url.config(
+        'ENGINE': 'django.db.backends.postgresql'
         default=os.getenv("DATABASE_URL",  f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
         conn_max_age=600,
         #ssl_require=True
